af2a37e881282843dd0d65a2cd936e25d1067849
Pranav Saraswat <85411336+Pranav-Saraswat@users.noreply.github.com>
2023-04-22 00:06:50 +0530

    update enums

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -9,6 +9,7 @@
 #
 
 from pyrogram import filters
+from pyrogram.enums import ChatType
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
@@ -90,7 +91,7 @@ async def settings_back_markup(
         await CallbackQuery.answer()
     except:
         pass
-    if CallbackQuery.message.chat.type == "private":
+    if CallbackQuery.message.chat.type == ChatType.PRIVATE:
         try:
             await app.resolve_peer(OWNER_ID[0])
             OWNER = OWNER_ID[0]
